2b0b1b46b65806a31062cfb1aa2c1e133b5b3777
SuryaVeda <suryaveda@hotmail.com>
2020-12-05 19:41:17 +0530

    x

--- aaa/aaa/settings.py
+++ aaa/aaa/settings.py
@@ -50,6 +50,8 @@ if DEBUG:
     ALLOWED_HOSTS = []
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATIC_URL = '/static/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    MEDIA_URL = '/media/'
 
 else:
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
