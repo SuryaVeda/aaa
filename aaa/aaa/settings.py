438655f1f23897db666ab7b36dafe552d2415e0d
SuryaVeda <suryaveda@hotmail.com>
2020-12-06 10:58:53 +0530

    x

--- aaa/aaa/settings.py
+++ aaa/aaa/settings.py
@@ -189,6 +189,8 @@ EMAIL_HOST_USER = 'suryaaddu@gmail.com'
 EMAIL_HOST_PASSWORD = 'Surya@@1997'
 SECURE_BROWSER_XSS_FILTER = True
 X_FRAME_OPTIONS = 'DENY'
+LOGOUT_REDIRECT_URL = reverse_lazy('account:logout')
+LOGIN_REDIRECT_URL=reverse_lazy('account:login')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
