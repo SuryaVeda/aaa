286562a2009f067b4f0cf95586cf97ab0cd6f1c4
SuryaVeda <suryaveda@hotmail.com>
2020-12-06 11:39:02 +0530

    x

--- aaa/aaa/urls.py
+++ aaa/aaa/urls.py
@@ -13,12 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.shortcuts import redirect
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from django.conf import settings
 from django.views.static import serve
 from django.contrib.auth import views
+from django.contrib.auth import authenticate, login
+def newlogin(request):
+    return redirect('accounts:login')
 
 
 urlpatterns = [
