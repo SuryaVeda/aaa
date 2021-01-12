eaa8b11aee2f7b6c26ac4a8bf2a1ff942060cc52
SuryaVeda <suryaaddu@gmail.com>
2021-01-12 23:31:20 +0530

    x

--- aaa/accounts/views.py
+++ aaa/accounts/views.py
@@ -44,7 +44,7 @@ def login_view(request):
                                     password=request.POST.get('password'))
                 if user is not None:
                     login(request, user)
-                    return redirect('archives:lectures')
+                    return redirect('home:home')
                 else:
                     messages.error(request, "Credentials are either wrong or not registered.")
                     print('see try statemetn')
