cc4eaad0aaeb13e778befa1a31283b4044514e86
SuryaVeda <suryaaddu@gmail.com>
2021-01-27 13:16:53 +0530

    x

--- aaa/home/views.py
+++ aaa/home/views.py
@@ -25,7 +25,9 @@ class Manage(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data( **kwargs)
         if self.request.user.is_staff:
-            context['users'] = User.objects.all()
+            context['users'] = User.objects.order_by('-pk')
+            context['users_length'] = len(context['users'])
+
         return context
 def email(request):
     subject = 'You visited conference page'
